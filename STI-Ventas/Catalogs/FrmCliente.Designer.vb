ca6e6565e5fb8255a76200517376ccccf4908aa3
Freddy <alek_886@hotmail.com>
2022-02-15 01:45:24 -0300

    formulario Clientes

---
+++ STI-Ventas/Catalogs/FrmCliente.Designer.vb
@@ -0,0 +1,158 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FrmCliente
+    Inherits FrmBase
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtId = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtRut = New System.Windows.Forms.TextBox()
+        Me.txtNombre = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.txtDireccion = New System.Windows.Forms.TextBox()
+        Me.txtCupo = New System.Windows.Forms.TextBox()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'btnSave
+        '
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.txtCupo)
+        Me.GroupBox1.Controls.Add(Me.txtDireccion)
+        Me.GroupBox1.Controls.Add(Me.Label5)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.txtNombre)
+        Me.GroupBox1.Controls.Add(Me.txtRut)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.txtId)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Size = New System.Drawing.Size(673, 128)
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(39, 27)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(51, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Id Cliente"
+        '
+        'txtId
+        '
+        Me.txtId.Location = New System.Drawing.Point(116, 24)
+        Me.txtId.Name = "txtId"
+        Me.txtId.Size = New System.Drawing.Size(60, 20)
+        Me.txtId.TabIndex = 1
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(39, 63)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(24, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Rut"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(39, 96)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(44, 13)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "Nombre"
+        '
+        'txtRut
+        '
+        Me.txtRut.Location = New System.Drawing.Point(116, 60)
+        Me.txtRut.Name = "txtRut"
+        Me.txtRut.Size = New System.Drawing.Size(124, 20)
+        Me.txtRut.TabIndex = 4
+        '
+        'txtNombre
+        '
+        Me.txtNombre.Location = New System.Drawing.Point(116, 96)
+        Me.txtNombre.Name = "txtNombre"
+        Me.txtNombre.Size = New System.Drawing.Size(353, 20)
+        Me.txtNombre.TabIndex = 5
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(277, 27)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(52, 13)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "Direccion"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(297, 60)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(32, 13)
+        Me.Label5.TabIndex = 7
+        Me.Label5.Text = "Cupo"
+        '
+        'txtDireccion
+        '
+        Me.txtDireccion.Location = New System.Drawing.Point(352, 24)
+        Me.txtDireccion.Name = "txtDireccion"
+        Me.txtDireccion.Size = New System.Drawing.Size(309, 20)
+        Me.txtDireccion.TabIndex = 8
+        '
+        'txtCupo
+        '
+        Me.txtCupo.Location = New System.Drawing.Point(352, 60)
+        Me.txtCupo.Name = "txtCupo"
+        Me.txtCupo.Size = New System.Drawing.Size(117, 20)
+        Me.txtCupo.TabIndex = 9
+        '
+        'FrmCliente
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(673, 345)
+        Me.Name = "FrmCliente"
+        Me.Text = "FrmCliente"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtNombre As TextBox
+    Friend WithEvents txtRut As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtId As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents txtCupo As TextBox
+    Friend WithEvents txtDireccion As TextBox
+End Class
---
+++ STI-Ventas/Catalogs/FrmCliente.vb
@@ -0,0 +1,238 @@
+﻿Imports STIVentas.Controller
+Imports STIVentas.Model
+
+Public Class FrmCliente
+
+
+
+
+
+
+
+    Protected Function ValidateWrite() As Boolean
+
+        Dim ret As Boolean = True
+
+        If String.IsNullOrEmpty(txtId.Text) Then
+            ret = CheckFailed("Debe completar el id de Cliente antes de continuar")
+        End If
+
+        Return ret
+    End Function
+
+
+
+
+
+
+    '' nuevo cod
+
+    ''' <summary>
+    ''' Se debe sobreescribir el metodo por cada tabla
+    ''' </summary>
+    ''' <returns>True si elimino</returns>
+    ''' <remarks>31.01.2021 jorge.nin92@gmail.com: Se crea el metodo</remarks>
+    Protected Friend Overrides Function DeleteRecord() As Boolean
+        Dim deleted As Boolean = False
+        Dim uomController As ClienteController
+
+        Try
+            Cursor = Cursors.WaitCursor
+            uomController = New ClienteController()
+            deleted = uomController.Delete(GetCurrentTable())
+
+            If Not deleted Then
+                HandleException(uomController.LastError)
+            End If
+
+        Catch ex As Exception
+            HandleException(ex)
+        Finally
+            Cursor = Cursors.Default
+        End Try
+
+        Return deleted
+
+    End Function
+
+    ''' <summary>
+    ''' Se debe sobreescribir el metodo por cada tabla
+    ''' </summary>
+    ''' <returns>Devuelve el identificador para el registrro</returns>
+    ''' <remarks>31.01.2021 jorge.nin92@gmail.com: Se crea el metodo</remarks>
+    Protected Friend Overrides Function GetRecordIdentification() As String
+        Return txtId.Text
+    End Function
+
+    ''' <summary>
+    ''' Se debe sobreescribir el metodo por cada tabla
+    ''' </summary>
+    ''' <returns>True si inserto</returns>
+    ''' <remarks>31.01.2021 jorge.nin92@gmail.com: Se crea el metodo</remarks>
+    Protected Friend Overrides Function InsertRecord() As Boolean
+        Dim ret As Boolean = False
+        Dim uomController As ClienteController
+
+        Try
+            Cursor = Cursors.WaitCursor
+            uomController = New ClienteController()
+            ret = uomController.Insert(GetCurrentTable())
+
+            If Not ret Then
+                HandleException(uomController.LastError)
+            End If
+
+        Catch ex As Exception
+            HandleException(ex)
+        Finally
+            Cursor = Cursors.Default
+        End Try
+
+        Return ret
+    End Function
+
+    ''' <summary>
+    ''' Se debe sobreescribir el metodo por cada tabla
+    ''' </summary>
+    ''' <returns>True si actualizo</returns>
+    ''' <remarks>31.01.2021 jorge.nin92@gmail.com: Se crea el metodo</remarks>
+    Protected Friend Overrides Function UpdateRecord() As Boolean
+        Dim ret As Boolean = False
+        Dim dbController As ClienteController
+
+        Try
+            If ValidateWrite() Then
+                Cursor = Cursors.WaitCursor
+                dbController = New ClienteController()
+                ret = dbController.Update(GetCurrentTable())
+
+                If Not ret Then
+                    HandleException(dbController.LastError)
+                End If
+            End If
+
+        Catch ex As Exception
+            HandleException(ex)
+        Finally
+            Cursor = Cursors.Default
+        End Try
+
+        Return ret
+    End Function
+
+    ''' <summary>
+    ''' Se debe sobreescribir el metodo por cada tabla
+    ''' </summary>
+    ''' <remarks>31.01.2021 jorge.nin92@gmail.com: Se crea el metodo</remarks>
+    Protected Friend Overrides Sub ClearFields()
+        txtId.Clear()
+        txtRut.Clear()
+        txtNombre.Clear()
+        txtDireccion.Clear()
+        txtCupo.Clear()
+        txtId.Enabled = True
+    End Sub
+
+    ''' <summary>
+    ''' Se debe sobreescribir para el metodo Load
+    ''' </summary>
+    ''' <remarks>31.01.2021 jorge.nin92@gmail.com: Se crea el metodo</remarks>
+    Protected Overrides Sub OnFormLoaded()
+        MyBase.OnFormLoaded()
+        AddHandler txtId.Enter, AddressOf TextBox_Enter
+        AddHandler txtRut.Enter, AddressOf TextBox_Enter
+        AddHandler txtNombre.Enter, AddressOf TextBox_Enter
+
+        AddHandler txtDireccion.Enter, AddressOf TextBox_Enter
+        AddHandler txtCupo.Enter, AddressOf TextBox_Enter
+
+    End Sub
+
+    Public Overrides Function GetCurrentTable() As IDBTable
+        Dim dbTable As New ClienteModel With {
+            .Id = 0,
+            .Rut = txtRut.Text,
+            .Nombre = txtNombre.Text,
+            .Direccion = txtDireccion.Text,
+            .Cupo = Convert.ToInt32(txtCupo.Text)
+        }
+
+        Return dbTable
+    End Function
+
+    ''' <summary>
+    ''' Maneja el evento clic del grig
+    ''' </summary>
+    ''' <param name="e">Evento que se detona</param>
+    ''' <remarks>31.01.2021 jorge.nin92@gmail.com: Se crea el metodo</remarks>
+    Protected Overrides Sub OnSelectedRow(ByVal e As DataGridViewCellEventArgs)
+        If e.RowIndex >= 0 Then
+
+            txtId.Text = dtGridView.Item(0, e.RowIndex).Value
+            txtRut.Text = dtGridView.Item(1, e.RowIndex).Value
+            txtNombre.Text = dtGridView.Item(2, e.RowIndex).Value
+            txtDireccion.Text = dtGridView.Item(3, e.RowIndex).Value
+            txtCupo.Text = dtGridView.Item(4, e.RowIndex).Value
+            isNewRecord = False
+            txtId.Enabled = False
+        End If
+    End Sub
+
+    ''' <summary>
+    ''' Se debe sobreescribir para cargar el grid
+    ''' </summary>
+    ''' <remarks>31.01.2021 jorge.nin92@gmail.com: Se crea el metodo</remarks>
+    Protected Overrides Sub LoadRecords()
+        Dim dbController As ClienteController
+        Dim records As List(Of ClienteModel)
+
+        Try
+            Cursor = Cursors.WaitCursor
+
+            If dtGridView.Columns().Count < 1 Then
+                dtGridView.Columns.Add(New DataGridViewTextBoxColumn With {.Name = "Id", .HeaderText = "Id"})
+                dtGridView.Columns.Add(New DataGridViewTextBoxColumn With {.Name = "Rut", .HeaderText = "Rut"})
+                dtGridView.Columns.Add(New DataGridViewTextBoxColumn With {.Name = "Nombre", .HeaderText = "Nombre"})
+                dtGridView.Columns.Add(New DataGridViewTextBoxColumn With {.Name = "Direccion", .HeaderText = "Direccion"})
+                dtGridView.Columns.Add(New DataGridViewTextBoxColumn With {.Name = "Cupo", .HeaderText = "Cupo"})
+
+            End If
+
+            dbController = New ClienteController()
+            records = dbController.GetList()
+
+            'dtGridView.DataSource = records
+            dtGridView.Rows.Clear()
+
+            For Each model As ClienteModel In records
+                dtGridView.Rows().Add(model.Id, model.Rut, model.Nombre, model.Direccion, model.Cupo)
+            Next
+
+            If records.Count < 1 And Not String.IsNullOrEmpty(dbController.LastError) Then
+                HandleException(dbController.LastError)
+                dtGridView.Rows.Add()
+                dtGridView.Rows.Add()
+            End If
+        Catch ex As Exception
+            HandleException(ex)
+        Finally
+            Cursor = Cursors.Default
+        End Try
+
+    End Sub
+
+    ''' <summary>
+    ''' Indica si debe haber interacción con el usuario para eliminar un registro
+    ''' </summary>
+    ''' <returns>True si actualizo</returns>
+    ''' <remarks>31.01.2021 jorge.nin92@gmail.com: Se crea el metodo</remarks>
+    Protected Friend Overrides Function AskToDelete() As Boolean
+        Return ConfirmDeleteRecord(GetRecordIdentification())
+    End Function
+
+
+
+    Private Sub btnSave_Click(sender As Object, e As EventArgs) Handles btnSave.Click
+
+    End Sub
+End Class
