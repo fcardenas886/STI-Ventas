4dd82ee5b01880c4f7da57072b03b8b3e6c4d118
Freddy <alek_886@hotmail.com>
2022-03-15 22:07:27 -0300

    ModeloPermisos

---
+++ STIVentas.Model/PermisosModel.vb
@@ -0,0 +1,36 @@
+﻿Imports STIVentas.Model
+
+Public Class PermisosModel : Implements IDBTable
+
+    Private RecordId As Long
+    Public Property IdUsuario As Integer
+    Public Property IdModulo As Integer
+    Public Property Insertar As Boolean
+    Public Property Editar As Boolean
+    Public Property Eliminar As Boolean
+
+
+    Public Property Id As Long Implements IDBTable.RecordId
+        Get
+            Return RecordId
+        End Get
+        Set(value As Long)
+            RecordId = value
+        End Set
+    End Property
+
+    Sub New()
+        RecordId = 0
+    End Sub
+
+    Sub New(ByVal Id_ As Integer, ByVal IdUsuario_ As Integer, ByVal IdModulo_ As Integer, ByVal Insertar_ As Boolean, ByVal Editar_ As Boolean, ByVal Eliminar_ As Boolean)
+
+        Id = Id_
+        IdUsuario = IdUsuario_
+        IdModulo = IdModulo_
+        Insertar = Insertar_
+        Editar = Editar_
+        Eliminar = Eliminar_
+
+    End Sub
+End Class
